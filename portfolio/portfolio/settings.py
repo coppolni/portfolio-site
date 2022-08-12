3ddc8ba6b11ab298a4513fc2098a0eb0ce9c6030
coppolni <nick.coppola2010@gmail.com>
2022-08-11 22:48:15 -0400

    Set Debug to False

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -30,7 +30,7 @@ with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = []
